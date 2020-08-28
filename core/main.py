906b5d87ba3009d1b17ddfde2a23c1fd5406c9bc
iMac <imac@iMac-iMac.local>
2020-08-28 13:27:11 +0300

    Another changes to imports

--- core/main.py
+++ core/main.py
@@ -18,11 +18,11 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from .security import authenticate, identity
-from resources.user import UserRegister
-from resources.item import Item, ItemList
-from resources.store import Store, StoreList
+from core.resources.user import UserRegister
+from core.resources.item import Item, ItemList
+from core.resources.store import Store, StoreList
 
-from db import db
+from core.db import db
 
 app = Flask (__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
