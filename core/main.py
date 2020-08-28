c0506c131e5d283481a0d67acaf8f3a7cc7844ac
iMac <imac@iMac-iMac.local>
2020-08-28 13:56:44 +0300

    Another changes to imports

--- core/main.py
+++ core/main.py
@@ -22,7 +22,6 @@ from .resources.user import UserRegister
 from .resources.item import Item, ItemList
 from .resources.store import Store, StoreList
 
-from .db import db
 
 app = Flask (__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
@@ -52,6 +51,7 @@ api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
+    from .db import db
     db.init_app(app)
     app.run(port=5000, debug = True)
 
